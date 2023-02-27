bfb4f6c557e84658104131759ae1f99020a1a06d
md-arif-islam <arifislamarif344@gmail.com>
2023-02-27 12:26:04 +0600

    v2.0

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,23 @@
 $(document).ready(function () {
-  $("#hamburger i").on("click", function () {
-    $("#sideber").css({ left: "0px" });
+  $(".delete").on("click", function () {
+    return confirm("Are you sure you want to delete this item?");
   });
 
-  $("#dismiss i").on("click", function () {
-    $("#sideber").css({ left: "-500px" });
+  $("#pwd").on("click", function () {
+    $(this).toggleClass("fa-eye fa-eye-slash");
+    var input = $("#pwdinput");
+    if (input.attr("type") === "password") {
+      input.attr("type", "text");
+    } else {
+      input.attr("type", "password");
+    }
+    input.focus();
+  });
+
+  $("#pimg").on("click", function () {
+    $("#pimgi").click();
+  });
+  $(".pimgedit").on("click", function () {
+    $("#pimgi").click();
   });
 });
